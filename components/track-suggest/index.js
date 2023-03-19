64850fc2c5721659bb51bd816e04c969f0847803
Yasin ilkalp Arabacı <yasinilkalp_24@hotmail.com>
2023-03-20 00:13:30 +0300

    updates

--- components/track-suggest/index.js
+++ components/track-suggest/index.js
@@ -57,7 +57,7 @@ const style = {
     top: '50%',
     left: '50%',
     transform: 'translate(-50%, -50%)',
-    width: 500,
+    minWidth: 400,
     bgcolor: 'background.paper',
     border: '1px solid #000',
     boxShadow: 24,
